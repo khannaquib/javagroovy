4978964232b55ffcc167f35c2016623e03639268
aquib khan <kaquib279@gmail.com>
2026-01-24 22:16:41 +0530

    pushing image to dockerhub

--- java.groovy
+++ java.groovy
@@ -1,22 +1,42 @@
 pipeline {
     agent any
+    environment {
+        IMAGE_NAME = "pythonapp"
+        DOCKERHUB_REPO = "aquibkhan/pythonapp"
+    }
+
     
     stages {
         stage('Checkout') {
             steps {
                 // Fetch the repository using SSH credentials
                 git branch: 'main', url: 'https://github.com/khannaquib/finalproject.git'
-                sh "scp -r * ubuntu@43.204.147.221:/home/ubuntu"
+                // sh "scp -r * ubuntu@:/home/ubuntu"
             }
        
         }
         stage('creating a docker image') {
             steps {
                 sshagent(['docker-server']) {
-                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@43.204.147.221 "docker build -t pythonapp:1 ."'
+                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.108.63.202 "docker build -t pythonapp ."'
                      }
                 }
-        }         
-    }
+        }
+        stage('Tag Image') {
+            steps {
+                sh 'docker tag ${IMAGE_NAME} ${aquibkhan456/pythonapp}:$BUILD_ID'
+            }
+        }
+
+        stage('Docker Hub Login') {
+            withCredentials([string(credentialsId: 'aquibkhan456', variable: 'dockerpass')]) {
+        sh 'docker login -u aquibkhan456 -p ${docker}'
+        sh 'docker image push aquibkhan456/${IMAGE_NAME}:v1.$BUILD_ID'
+        sh 'docker image push aquibkhan456/${IMAGE_NAME}:latest'
+        }
+        
+        }
+                 
+ }
     
 }
