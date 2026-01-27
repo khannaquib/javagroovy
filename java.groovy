86e0d055f2f4552fa7d0ad53367bb3b08fcc1347
aquib khan <kaquib279@gmail.com>
2026-01-27 20:14:37 +0530

    using different code

--- java.groovy
+++ java.groovy
@@ -1,43 +1,43 @@
 pipeline {
     agent any
+
     environment {
         IMAGE_NAME = "pythonapp"
-        DOCKERHUB_REPO = "aquibkhan/pythonapp"
+        DOCKERHUB_REPO = "aquibkhan456/pythonapp"
     }
 
-    
     stages {
+
         stage('Checkout') {
             steps {
-                // Fetch the repository using SSH credentials
-                git branch: 'main', url: 'https://github.com/khannaquib/finalproject.git'
-                // sh "scp -r * ubuntu@:/home/ubuntu"
+                git branch: 'main',
+                    url: 'https://github.com/khannaquib/finalproject.git'
             }
-       
         }
+
         stage('creating a docker image') {
             steps {
                 sshagent(['docker-server']) {
-                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker build -t pythonapp ."'
-                     }
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker build -t pythonapp ."'
                 }
-        }
-        
-        stage('Docker Hub Login') {
-            steps{
-                sshagent(['docker-server']) {
-                  sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker tag ${IMAGE_NAME} ${aquibkhan456/pythonapp}:$BUILD_ID"' 
-                                        
-                 withCredentials([string(credentialsId: 'aquibkhan456', variable: 'dockerpass')]) {
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker login -u aquibkhan456 -p ${dockerpass}"'
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker image push aquibkhan456/${IMAGE_NAME}:v1.$BUILD_ID"'
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker image push aquibkhan456/${IMAGE_NAME}:latest"'
-             }
             }
         }
-        
+
+        stage('Docker Hub Login') {
+            steps {
+                sshagent(['docker-server']) {
+
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker tag ${IMAGE_NAME} ${DOCKERHUB_REPO}:$BUILD_ID"'
+
+                    withCredentials([
+                        string(credentialsId: 'aquibkhan456', variable: 'dockerpass')
+                    ]) {
+                        sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker login -u aquibkhan456 -p ${dockerpass}"'
+                        sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker image push ${DOCKERHUB_REPO}:$BUILD_ID"'
+                        sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.113.239 "docker image push ${DOCKERHUB_REPO}:latest"'
+                    }
+                }
+            }
         }
-                 
- }
-    
+    }
 }
